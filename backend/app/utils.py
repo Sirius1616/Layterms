0f6876aeae087094cd09f2c7e3534df1cbd40dec
Sirius1616 <siriusa1.615@gmail.com>
2026-07-29 16:25:40 +0100

    Created utils for account processes and sending mails

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -1,13 +1,99 @@
 import jwt
+import emails
+import logging
 from typing import Any
+from pathlib import Path
+from jinja2 import Template
 from pydantic import EmailStr
+from datetime import datetime
+from dataclasses import dataclass
 from app.core.config import settings
-from datetime import datetime, timedelta
-from app.models import Token, TokenPayload
 from jwt.exceptions import InvalidTokenError
 
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
+@dataclass
+class EmailData:
+    html_content: str
+    subject: str
+
+
+def render_email_template(template_name: str, context: dict[str] | Any) -> str:
+    template_str = (Path(__file__).parent/"email-templates"/"build"/template_name).read_text
+    html_content = Template(template_str).render(context)
+    return html_content
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def send_email(email_to: str, subject: str = "", 
+               html_content: str = "") -> None:
+    assert settings.EMAIL_FROM_EMAIL
+    assert settings.enable_email
+
+    message = emails.message.Message(
+        subject=subject,
+        html=html_content,
+        mail_from=(settings.EMAIL_FROM_NAME, settings.EMAIL_FROM_EMAIL)
+    )
+    smtp_mail_options = {"smtp_host": settings.SMTP_HOST, "port": settings.SMTP_PORT}
+    if settings.SMTP_SSL:
+        smtp_mail_options["ssl"] = settings.SMTP_SSL
+    elif settings.SMTP_TLS:
+        smtp_mail_options["tls"] = settings.SMTP_TLS
+    if settings.SMTP_USER:
+        smtp_mail_options["user"] = settings.SMTP_USER
+    if settings.SMTP_PASSWORD:
+        smtp_mail_options["password"] = settings.SMTP_PASSWORD
+
+    response = message.send(to=email_to, smtp_mail_options=smtp_mail_options)
+    logger.info(f"send email result: {response}")
+
+
+def generate_test_email(email_to: str) -> EmailData:
+    project_name = settings.PROJECT_NAME
+    subject = f"{project_name} - Test email"
+    html_content = render_email_template(
+        template_name= "test_email.html",
+        context={"project_name": project_name, "email": email_to}
+    )
+    return EmailData(html_content=html_content, subject=subject)
+
+def generete_new_account_email(email_to: str, password: str, username: str):
+    project_name = settings.PROJECT_NAME
+    subject = f"{project_name} - New account for {username}"
+    context = {"project_name": project_name,
+               "username": username,
+               "password": password
+               }
+    html_content = render_email_template(
+            template_name="new_account.html",
+            context=context
+        )
+    return EmailData(
+        html_content=html_content,
+        subject=subject
+    )
+
+
+def generate_password_reset_email(email: str, email_to: str, token: str) -> EmailData:
+    project_name = settings.PROJECT_NAME
+    template_name = "reset_password.html"
+    subject = f"{project_name} - Password reset for {email}"
+    link = f"{settings.FRONTEND_HOST}/reset-password?token={token}"
+    context = {"project_name": project_name,
+               "username": email,
+               "email": email_to,
+               "valid_hours": settings.RESET_TOKEN_EXPIRE,
+               "link": link
+               }
+    html_content = render_email_template(
+        template_name=template_name, context=context
+    )
+    return EmailData(html_content=html_content, subject=subject)
 
 
 def create_password_reset_token(email : str) -> str:
